5072cc23ab972f0093aceb1add265a60bbb7a781
danandreea <deeadan99@yahoo.com>
2021-02-25 08:56:01 +0200

    Third Commit

---
+++ seminar1/src/classes/pack/Zookeeper.java
@@ -0,0 +1,31 @@
+package classes.pack;
+
+public class Zookeeper {
+	
+	
+	private String name;
+	
+	public Zookeeper(String name) {
+		super();
+		this.name = name;
+	}
+
+	
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public void feed(Animal a)
+	{
+		System.out.println("Animalul" + a.getName() + "a fost hranit");
+	}
+
+	
+	
+}
--- seminar1/src/main/pack/Main.java
+++ seminar1/src/main/pack/Main.java
@@ -1,10 +1,17 @@
 package main.pack;
 
+import classes.pack.Zoo;
+import classes.pack.Zookeeper;
+
 public class Main {
 
 	 public static void main(String[] args) 
 	    { 
-	        System.out.println("Welcome"); 
+	        Zookeeper keeper=new Zookeeper("Dani");
+	        Zoo zoo=new Zoo();
+	        zoo.setZookeeper(keeper);
+	        
+	        
 	      
 	    } 
 }
